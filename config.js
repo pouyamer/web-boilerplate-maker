44c9129d53d2b23c69d53e1398a7e261c9325e9d
Pouya Moazzezi <pmoazzezi@gmail.com>
2022-07-12 00:13:28 +0430

    changed output directory to build.

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   html: {
     name: "index",
     extension: "html", // "html" / htm
-    path: "./test",
+    path: "./build",
     title: "Document", // Title in the Title tag
     jsFileNames: ["script"], // Array of js files (file names) to include in the html
     cssFileNames: ["style"] // Array of css files (file names) to include in the html
@@ -10,13 +10,13 @@ const config = {
   css: {
     name: "style",
     extension: "css", // "css" / scss / sass
-    path: "./test",
+    path: "./build",
     type: "css-normal" // "css-normal" / css-reset (See types.js)
   },
   js: {
     name: "script",
     extension: "js", // "js" / ts
-    path: "./test",
+    path: "./build",
     type: "js-normal" // "js-normal" / js-canvas (See types.js)
   }
 }
